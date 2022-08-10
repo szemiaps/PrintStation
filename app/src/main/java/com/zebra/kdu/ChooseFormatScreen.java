6bb4d1c99daaf1cd35f456dd5223c16ee45afd92
szemiaps <szemiaps@gmail.com>
2022-08-10 20:39:11 +0200

    some more comment

--- app/src/main/java/com/zebra/kdu/ChooseFormatScreen.java
+++ app/src/main/java/com/zebra/kdu/ChooseFormatScreen.java
@@ -78,6 +78,7 @@ import com.zebra.sdk.printer.discovery.UsbDiscoverer;
 
 public class ChooseFormatScreen extends ListActivity implements FinishInfo {
 	/*CONSTANTS*/
+	/*ad some more comment*/
 	public static final String APP_PREFERENCES_KEY = "PrintStationPreferences";
 	public static final int DIALOG_DISCOVERY = 0;
 	public static final int DIALOG_ABOUT = 1;
